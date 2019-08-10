c33fc3f7c3188bba12d4bd0fff193097444f56b0
Dathan Bennett <dathan@gmail.com>
2019-08-10 13:59:58 -0700

    Updates

---
+++ precache-manifest.2e2c969b2fcaf92cfc9f96bcb3905b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d9a67a058a79d65b5cd652e6b939fd",
+    "url": "/mars-clock/index.html"
+  },
+  {
+    "revision": "9885a26294aebadf5bdd",
+    "url": "/mars-clock/static/css/main.19d71058.chunk.css"
+  },
+  {
+    "revision": "b6a8672679560fb0e3fb",
+    "url": "/mars-clock/static/js/2.3be312b4.chunk.js"
+  },
+  {
+    "revision": "9885a26294aebadf5bdd",
+    "url": "/mars-clock/static/js/main.c77b5a98.chunk.js"
+  },
+  {
+    "revision": "7646267b599e16e6c343",
+    "url": "/mars-clock/static/js/runtime~main.79f752e9.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/mars-clock/static/media/digital-7-mono.58045dab.ttf"
+  }
+]);
